b26de7cfd1f6b8235e356f671ee4a53ad99c05c8
nathan <kangninathan@gmail.com>
2026-03-11 16:07:19 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, render_template
 from tester.runner import run_tests
+from storage import init_db, save_run, list_runs
 
 app = Flask(__name__)
+
+# Initialiser la base SQLite au démarrage
+init_db()
 
 @app.route("/")
 def home():
@@ -14,12 +18,21 @@ def home():
 @app.route("/run")
 def run():
     results = run_tests()
+
+    # Sauvegarder le run dans SQLite
+    save_run(results)
+
     return jsonify(results)
 
 @app.route("/dashboard")
 def dashboard():
-    results = run_tests()
-    return render_template("dashboard.html", results=results)
+
+    runs = list_runs()
+
+    return render_template(
+        "dashboard.html",
+        runs=runs
+    )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
